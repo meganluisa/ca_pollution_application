6223a1cdd89ac54758501c3a5e1a3239fa112df1
clivefrog <geddescol@gmail.com>
2020-10-05 15:21:38 +1000

    full screen map, slidernav

--- templates/main.js
+++ templates/main.js
@@ -1,1 +1,11 @@
 /* <script src="//unpkg.com/leaflet/dist/leaflet.js"></script> */
+function openNav() {
+    document.getElementById("sidenav").style.width = "250px";
+    document.getElementById("years").style.left = "270px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("sidenav").style.width = "0";
+    document.getElementById("years").style.left = "20px";
+}
