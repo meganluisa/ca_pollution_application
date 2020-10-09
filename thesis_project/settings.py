e47c96d89a017e629247f2ebf5bb51c6dda5115c
Megan White <meganluisawhite@gmail.com>
2020-10-09 16:39:03 -0600

    changed settings after adding venv path

--- thesis_project/settings.py
+++ thesis_project/settings.py
@@ -132,6 +132,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+import django_heroku
+django_heroku.settings(locals())
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
